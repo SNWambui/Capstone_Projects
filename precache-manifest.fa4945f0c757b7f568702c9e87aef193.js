5f99f1009bbd1b827942dedafd1a2c1aff497d3d
Stevedavies Wambui <44650523+SNWambui@users.noreply.github.com>
2023-01-16 16:43:54 +0530

    Updates

---
+++ precache-manifest.fa4945f0c757b7f568702c9e87aef193.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4fc7cc4d7e38bf7015a9",
+    "url": "/crop-decision-hub/static/css/main.c3e39018.chunk.css"
+  },
+  {
+    "revision": "4fc7cc4d7e38bf7015a9",
+    "url": "/crop-decision-hub/static/js/main.4fc7cc4d.chunk.js"
+  },
+  {
+    "revision": "046e05133628af1d8cc8",
+    "url": "/crop-decision-hub/static/js/1.046e0513.chunk.js"
+  },
+  {
+    "revision": "0669c0b864044230aa5d",
+    "url": "/crop-decision-hub/static/js/2.0669c0b8.chunk.js"
+  },
+  {
+    "revision": "80cf7803b74645b4873e",
+    "url": "/crop-decision-hub/static/js/runtime~main.80cf7803.js"
+  },
+  {
+    "revision": "2c3c4d55d03e896d583a6e6e638061a2",
+    "url": "/crop-decision-hub/index.html"
+  }
+];
