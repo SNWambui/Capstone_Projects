2fe3c5bbc12dd3d139efda1bf54f1458ca344f74
Stevedavies Wambui <44650523+SNWambui@users.noreply.github.com>
2023-02-17 16:58:27 +0530

    added django-heroku and psycog2 to requirements.txt, changed python runtime to 3.10.10, added django_heroku in settings.py for heroku hosting and set whitenoise middelware and staticfiles_storeage

--- farmer_weather/farmer_weather/settings.py
+++ farmer_weather/farmer_weather/settings.py
@@ -13,6 +13,7 @@ import os
 import environ
 
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 # BASE_DIR = Path(__file__).resolve().parent.parent
@@ -158,6 +159,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = (
     'whitenoise.storage.CompressedManifestStaticFilesStorage')
 
+
+# heroku hosting requirement
+django_heroku.settings(locals())
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
